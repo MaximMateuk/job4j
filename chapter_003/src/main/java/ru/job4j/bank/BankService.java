ab1051a4abea7a387ceb8f444acc0be00f9f6744
mmateiuk <mmateiuk@cybervisiontech.com>
2020-07-06 11:03:48 +0300

    task-140349 - Bank transfers added

---
+++ chapter_003/src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,66 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        if (user != null && findByRequisite(passport, account.getRequisite()) == null) {
+            users.get(user).add(account);
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            for (Account account : users.get(user)) {
+                if (account.getRequisite().equals(requisite)) {
+                    return account;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite, String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        if (srcAccount == null || srcAccount.getBalance() < amount) {
+            rsl = false;
+        }
+        Account destAcount = findByRequisite(destPassport, destRequisite);
+        if (destAcount != null) {
+            double beforeBalance = destAcount.getBalance();
+            destAcount.setBalance(beforeBalance + amount);
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        List<Account> accounts = new ArrayList<>();
+        String requisite = "3fdsbb9";
+        accounts.add(new Account("3fdsbb9", 140));
+        int index = accounts.indexOf(new Account(requisite, -1));
+        Account find = accounts.get(index);
+        System.out.println(find.getRequisite() + " -> " + find.getBalance());
+    }
+}
