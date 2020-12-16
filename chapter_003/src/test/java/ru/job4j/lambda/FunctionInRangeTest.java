eb775a06176bf5d60cc3f9499b694d5c9021ae55
mmateiuk <mmateiuk@cybervisiontech.com>
2020-12-16 18:45:14 +0200

    task-140239 - add tests

--- chapter_003/src/test/java/ru/job4j/lambda/FunctionInRangeTest.java
+++ chapter_003/src/test/java/ru/job4j/lambda/FunctionInRangeTest.java
@@ -16,4 +16,20 @@ public class FunctionInRangeTest {
         List<Double> expected = Arrays.asList(11D, 13D, 15D);
         assertThat(result, is(expected));
     }
+
+    @Test
+    public void whenSquaredFunctionFromFiveToEight() {
+        FunctionInRange calculation = new FunctionInRange();
+        List<Double> result = calculation.diapason(5, 8, x -> x * x);
+        List<Double> expected = Arrays.asList(25D, 36D, 49D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenLogFromFiveToEight() {
+        FunctionInRange calculation = new FunctionInRange();
+        List<Double> result = calculation.diapason(5, 8, Math::log);
+        List<Double> expected = Arrays.asList(1.6094379124341003D, 1.791759469228055D, 1.9459101490553132D);
+        assertThat(result, is(expected));
+    }
 }
