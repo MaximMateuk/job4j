bd772dea19e308fb1c984746cd577cbac5b6e151
mmateiuk <mmateiuk@cybervisiontech.com>
2021-05-31 10:54:07 +0300

    task-110228#140251 - change to Stream.of()

--- chapter_004/src/main/java/ru/job4j/stream/MatrixConverter.java
+++ chapter_004/src/main/java/ru/job4j/stream/MatrixConverter.java
@@ -3,6 +3,7 @@ package ru.job4j.stream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MatrixConverter {
 
@@ -12,6 +13,7 @@ public class MatrixConverter {
     }
 
     public static List<Integer> convert(Integer[][] matrix) {
-        return Arrays.stream(matrix).flatMap(Arrays::stream).collect(Collectors.toList());
+
+        return Stream.of(matrix).flatMap(Arrays::stream).collect(Collectors.toList());
     }
 }
